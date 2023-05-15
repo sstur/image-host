067d544dd1a99a0501aefcc4377302fa70a045e8
Simon <sstur@me.com>
2023-05-15 12:50:45 -0700

    Add express

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,19 @@
 import './env';
 
-// eslint-disable-next-line no-console
-console.log('Hello world');
+import express from 'express';
+import cors from 'cors';
+
+const PORT = 3000;
+
+const app = express();
+app.disable('x-powered-by');
+app.use(cors());
+
+app.get('/', (request, response) => {
+  response.send(`<p>Hello world</p>`);
+});
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Listening on http://localhost:${PORT}`);
+});
